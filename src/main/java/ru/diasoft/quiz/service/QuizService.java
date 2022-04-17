5b1d6d86d8f9d15b1d1148cac8c6b117ce0aa85d
vtsapko <vtsapko@diasoft.ru>
2022-04-17 22:15:28 +0400

    Revert "hw1"
    This reverts commit d38f9ad5e9e2101c702d73b899c84f8e589887d4.

--- src/main/java/ru/diasoft/quiz/service/QuizService.java
+++
@@ -1,9 +0,0 @@
-package ru.diasoft.quiz.service;
-
-import org.springframework.core.io.Resource;
-
-public interface QuizService {
-    void startQuiz(Resource resource);
-
-    String getPath();
-}
